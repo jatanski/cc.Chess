b27fa6c0be8a6607073a695eee66c195955c05de
marcin.cyboran <marcin.cyboran.wsb@gmail.com>
2019-08-12 16:31:25 +0200

    Pawn moves & attacs + small changes

--- src/Game/pieces/pawn.js
+++ src/Game/pieces/pawn.js
@@ -1,17 +1,31 @@
-import Piece from "./piece";
+import Piece from './piece';
 
 class Pawn extends Piece {
   constructor(x, y, side) {
     super(x, y, side);
-    this.name = "pawn";
+    this.name = 'pawn';
     this.display = `<i class="fas fa-chess-pawn ${side}"></i>`;
+    this._vector = (this._side == 'white') ? -1 : 1; // 1 to góra -1 to dół
   }
 
   // główna metoda, w której trzeba zapisać wszystkie możliwe ruchy danej bierki.
   findLegalMoves() {
-    const possibleMoves = [];
+    const allMoves = Array([this._x + this._vector, this._y]);
 
-    return posiblesMoves;
+    return allMoves.filter(el => {
+      return !(el[0] > 7 || el[0] < 0);
+    });
+  }
+
+  findLegalAttacs() {
+    const allAttacs = Array(
+      [this._x + this._vector, this._y + 1],
+      [this._x + this._vector, this._y - 1]
+    );
+
+    return allAttacs.filter(el => {
+      return !(el[0] < 0 || el[0] > 7 || el[1] < 0 || el[1] > 7)
+    });
   }
 }
 
