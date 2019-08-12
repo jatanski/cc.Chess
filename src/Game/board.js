d4dc320846dc797ce50b6ec8ec248b87c7446652
Greg <gpr.witczak@gmail.com>
2019-08-12 12:45:35 +0200

    Knight possible moves

--- src/Game/board.js
+++ src/Game/board.js
@@ -1,4 +1,5 @@
 import Pawn from "./pieces/pawn";
+import Knight from "./pieces/knight"
 
 // klasa inicjująca tablicę, gdzie będą zapisywane pozycje bierek
 
@@ -16,6 +17,15 @@ board.createSecondLevel();
 // tutaj tworzycie nowe obiekty waszych bierek i od razu umieszczacie je na szachownicy
 let pawn = new Pawn(6, 0, "white");
 board[pawn._x][pawn._y] = pawn;
+
+let knight_w1 = new Knight(7, 1, "white");
+board[knight_w1._x][knight_w1._y] = knight_w1;
+let knight_w2 = new Knight(7, 6, "white");
+board[knight_w2._x][knight_w2._y] = knight_w2;
+let knight_b1 = new Knight(0, 1, "black");
+board[knight_b1._x][knight_b1._y] = knight_b1;
+let knight_b2 = new Knight(0, 6, "black");
+board[knight_b2._x][knight_b2._y] = knight_b2;
 
 console.log(board); // służy do podejrzenia tablicy w konsoli
 
