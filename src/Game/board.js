f760d7b01a4357f9a65ce9505ee57240450fd075
Kuba <ja.tanski@gmail.com>
2019-08-14 17:50:23 +0200

    change styles, add listeners to square & move working

--- src/Game/board.js
+++ src/Game/board.js
@@ -11,37 +11,49 @@ class Board extends Array {
     }
   }
 
+  // tutaj tworzycie nowe obiekty waszych bierek i od razu umieszczacie je na szachownicy
+
   createAndSetPawns(side) {
-    const row = (side === 'white') ? 1 : 6;
-    for(let i = 0; i < this[row].length; i++ ) {
+    const row = side === "white" ? 6 : 1;
+    for (let i = 0; i < this[row].length; i++) {
       this[row][i] = new Pawn(row, i, side);
     }
   }
 
+  createAndSetQueen(side) {
+    const row = side === "white" ? 7 : 0;
+    this[row][3] = new Queen(row, 3, side);
+  }
+
+  createAndSetKnight(side) {
+    const row = side === "white" ? 7 : 0;
+    const startPositionKnight = [1, 6];
+    for (let i = 0; i < startPositionKnight.length; i++) {
+      this[row][startPositionKnight[i]] = new Knight(
+        row,
+        startPositionKnight[i],
+        side
+      );
+    }
+  }
+
+  // metoda inicjująca
+
   init() {
     this.createSecondLevel();
-    this.createAndSetPawns('white');
-    this.createAndSetPawns('black');
+
+    const colors = ["white", "black"];
+
+    for (let i = 0; i < colors.length; i++) {
+      this.createAndSetPawns(colors[i]);
+      this.createAndSetQueen(colors[i]);
+      this.createAndSetKnight(colors[i]);
+    }
   }
 }
 
 const board = new Board(8);
 board.init();
-
-// tutaj tworzycie nowe obiekty waszych bierek i od razu umieszczacie je na szachownicy
-
-let knight_w1 = new Knight(7, 1, "white");
-board[knight_w1._x][knight_w1._y] = knight_w1;
-let knight_w2 = new Knight(7, 6, "white");
-board[knight_w2._x][knight_w2._y] = knight_w2;
-let knight_b1 = new Knight(0, 1, "black");
-board[knight_b1._x][knight_b1._y] = knight_b1;
-let knight_b2 = new Knight(0, 6, "black");
-board[knight_b2._x][knight_b2._y] = knight_b2;
-let queen_w1 = new Queen(7, 3, "white");
-board[queen_w1._x][queen_w1._y] = queen_w1;
-let queen_b1 = new Queen(0, 3, "black");
-board[queen_b1._x][queen_b1._y] = queen_b1;
 
 console.log(board); // służy do podejrzenia tablicy w konsoli
 
--- src/Game/pieces/pawn.js
+++ src/Game/pieces/pawn.js
@@ -1,11 +1,11 @@
-import Piece from './piece';
+import Piece from "./piece";
 
 class Pawn extends Piece {
   constructor(x, y, side) {
     super(x, y, side);
-    this.name = 'pawn';
-    this.display = `<i class="fas fa-chess-pawn ${side}"></i>`;
-    this._vector = (this._side == 'white') ? -1 : 1; // 1 to góra -1 to dół
+    this.name = "pawn";
+    this.display = `<i data-color=${side} class="fas fa-chess-pawn ${side}"></i>`;
+    this._vector = this._side == "white" ? -1 : 1; // 1 to góra -1 to dół
   }
 
   // główna metoda, w której trzeba zapisać wszystkie możliwe ruchy danej bierki.
@@ -24,7 +24,7 @@ class Pawn extends Piece {
     );
 
     return allAttacs.filter(el => {
-      return !(el[0] < 0 || el[0] > 7 || el[1] < 0 || el[1] > 7)
+      return !(el[0] < 0 || el[0] > 7 || el[1] < 0 || el[1] > 7);
     });
   }
 }
