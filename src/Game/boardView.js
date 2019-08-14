7970de844c022655126829282eaa977eff7c547c
Micavanco <micavanco@gmail.com>
2019-08-14 16:06:04 +0200

    Added If statements to check type of piece

--- src/Game/boardView.js
+++ src/Game/boardView.js
@@ -50,7 +50,17 @@ export default class BoardView {
             this.highlightSquares(position);
             this.prevElement = position;
 
-            
+            let className;
+            if(el.nodeName == "I")
+                className = el.classList[1];
+            else
+                className = el.childNodes[0].classList[1];
+
+            if(className === 'fa-chess-pawn') {
+
+            }else if(className === 'fa-chess-knight') {
+
+            }
         }
     }
 
