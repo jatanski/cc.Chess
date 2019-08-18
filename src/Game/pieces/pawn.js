f62038b305537c8a588c95e334cd7b8f3f6d1609
Kuba <ja.tanski@gmail.com>
2019-08-18 17:06:19 +0200

    fix promotePawn

--- src/Game/pieces/pawn.js
+++ src/Game/pieces/pawn.js
@@ -1,98 +1,100 @@
 import Piece from "./piece";
 // import board from './../board';
-import PromotePawn from './../promotePawn'
+import PromotePawn from "./../promotePawn";
 
 class Pawn extends Piece {
-    constructor(x, y, side) {
-        super(x, y, side);
-        this.name = "pawn";
-        this.display = `<i data-color=${side} class="fas fa-chess-pawn ${side}"></i>`;
-        this._vector = this._side == "white" ? -1 : 1; // 1 to góra -1 to dół
+  constructor(x, y, side) {
+    super(x, y, side);
+    this.name = "pawn";
+    this.display = `<i data-color=${side} class="fas fa-chess-pawn ${side}"></i>`;
+    this._vector = this._side == "white" ? -1 : 1; // 1 to góra -1 to dół
+  }
+
+  // Filtrowanie ruchów wykraczających poza szachownice
+  filterOutBoardMoves(possibleMoves) {
+    return possibleMoves.filter(el => {
+      return !(el[0] > 7 || el[0] < 0);
+    });
+  }
+
+  // Filtrowanie ruchów kolizyjnych (przy ruchu piona jest bez znaczenia czy to swój czy wróg)
+  // possibleMoves interface : [ [x, y] ] lub [ [x, y], [x, y] ]
+  filterCollisionMoves(possibleMoves, board) {
+    if (possibleMoves.length < 1) return possibleMoves;
+
+    let legalMoves = [];
+
+    for (let moveCords of possibleMoves) {
+      const x = moveCords[0];
+      const y = moveCords[1];
+
+      if (board[x][y]) {
+        break;
+      } else {
+        legalMoves.push(moveCords);
+      }
     }
 
-    // Filtrowanie ruchów wykraczających poza szachownice
-    filterOutBoardMoves(possibleMoves) {
-        return possibleMoves.filter(el => {
-            return !(el[0] > 7 || el[0] < 0);
-        });
+    return legalMoves;
+  }
+
+  _showAttackOnlyIfPossible(attacksArr, board) {
+    const allLegalAttacks = [];
+
+    for (let attackCords of attacksArr) {
+      const x = attackCords[0];
+      const y = attackCords[1];
+
+      if (board[x][y] && board[x][y]._side !== this._side) {
+        allLegalAttacks.push(attackCords);
+      }
     }
 
-    // Filtrowanie ruchów kolizyjnych (przy ruchu piona jest bez znaczenia czy to swój czy wróg)
-    // possibleMoves interface : [ [x, y] ] lub [ [x, y], [x, y] ]
-    filterCollisionMoves(possibleMoves, board) {
-        if (possibleMoves.length < 1 ) return possibleMoves;
+    return allLegalAttacks;
+  }
 
-        let legalMoves = [];
+  // główna metoda, w której trzeba zapisać wszystkie możliwe ruchy danej bierki.
+  findLegalMoves(board) {
+    let legalMoves;
+    const allMoves = Array([this._x + this._vector, this._y]);
 
-        for(let moveCords of possibleMoves) {
-            const x = moveCords[0];
-            const y = moveCords[1];
-
-            if (board[x][y]) {
-                break;
-            } else {
-                legalMoves.push(moveCords);
-            }
-        }
-
-        return legalMoves;
+    // Dla pierwszego ruchu możliwość ruchu o 2
+    if (this._pristine) {
+      allMoves.push([this._x + this._vector * 2, this._y]);
     }
 
-    _showAttackOnlyIfPossible(attacksArr, board) {
-        const allLegalAttacks = [];
-        
-        for (let attackCords of attacksArr) {
-            const x = attackCords[0];
-            const y = attackCords[1];
-            
-            if(board[x][y] && board[x][y]._side !== this._side) {
-                allLegalAttacks.push(attackCords);
-            }
-        }
+    legalMoves = this.filterOutBoardMoves(allMoves);
+    legalMoves = this.filterCollisionMoves(allMoves, board);
 
-        return allLegalAttacks;
+    return legalMoves;
+  }
+
+  findLegalAttacks(board) {
+    let legalAttacks;
+    const allAttacks = Array(
+      [this._x + this._vector, this._y + 1],
+      [this._x + this._vector, this._y - 1]
+    );
+
+    legalAttacks = allAttacks.filter(el => {
+      return !(el[0] < 0 || el[0] > 7 || el[1] < 0 || el[1] > 7);
+    });
+
+    legalAttacks = this._showAttackOnlyIfPossible(legalAttacks, board);
+
+    return legalAttacks;
+  }
+
+  // sprawdzenie pozycji po ruchu, okreslenie czy jest mozliwosc promocji
+  move(newPosition, board) {
+    super.move(newPosition);
+    if (
+      (this._side === "white" && this._x === 0) ||
+      (this._side === "black" && this._x === 7)
+    ) {
+      new PromotePawn(this, board);
     }
-
-    // główna metoda, w której trzeba zapisać wszystkie możliwe ruchy danej bierki.
-    findLegalMoves(board) {
-        let legalMoves;
-        const allMoves = Array([this._x + this._vector, this._y]);
-
-        // Dla pierwszego ruchu możliwość ruchu o 2
-        if(this._pristine) {
-            allMoves.push([this._x + (this._vector * 2), this._y]) 
-        }
-
-        legalMoves = this.filterOutBoardMoves(allMoves);
-        legalMoves = this.filterCollisionMoves(allMoves, board);
-
-        return legalMoves;
-    }
-
-    findLegalAttacks(board) {
-        let legalAttacks;
-        const allAttacks = Array(
-            [this._x + this._vector, this._y + 1],
-            [this._x + this._vector, this._y - 1]
-        );
-
-        legalAttacks = allAttacks.filter(el => {
-            return !(el[0] < 0 || el[0] > 7 || el[1] < 0 || el[1] > 7);
-        });
-
-        legalAttacks = this._showAttackOnlyIfPossible(legalAttacks, board);
-
-        return legalAttacks;
-    }
-
-    // sprawdzenie pozycji po ruchu, okreslenie czy jest mozliwosc promocji
-    move(newPosition) {
-        super.move(newPosition);
-        if ((this._side === 'white' && this._x === 0) ||
-            (this._side === 'black' && this._x === 7)) {
-            new PromotePawn(this)
-        }
-    }
+  }
 }
 
 export default Pawn;
--- src/Game/promotePawn.js
+++ src/Game/promotePawn.js
@@ -1,69 +1,70 @@
-import board from './board';
+// import board from "./board";
 import Rook from "./pieces/rook";
 import Knight from "./pieces/knight";
 import Queen from "./pieces/queen";
 import Bishop from "./pieces/bishop";
 
 class PromotePawn {
-  constructor(pawn) {
+  constructor(pawn, board) {
     this.piece = pawn;
     this.initChoices();
-    this.promote();
+    this.promote(board);
   }
 
   initChoices() {
     const side = this.piece._side;
 
-    const promotionBox = document.createElement('div');
-    promotionBox.className = 'promotion-box';
+    const promotionBox = document.createElement("div");
+    promotionBox.className = "promotion-box";
     promotionBox.style.color = side;
 
     const text = document.createTextNode("Promote your pawn!");
     promotionBox.appendChild(text);
 
-    const icons = document.createElement('div');
-    icons.className = 'promotion-icons';
+    const icons = document.createElement("div");
+    icons.className = "promotion-icons";
     promotionBox.appendChild(icons);
 
-    const promoteToRook = document.createElement('i');
+    const promoteToRook = document.createElement("i");
     promoteToRook.className = `fas fa-chess-rook ${side}`;
     promoteToRook.id = "rook";
-    const promoteToKnight = document.createElement('i');
+    const promoteToKnight = document.createElement("i");
     promoteToKnight.className = `fas fa-chess-knight ${side}`;
     promoteToKnight.id = "knight";
-    const promoteToBishop = document.createElement('i');
+    const promoteToBishop = document.createElement("i");
     promoteToBishop.className = `fas fa-chess-bishop ${side}`;
     promoteToBishop.id = "bishop";
-    const promoteToQueen = document.createElement('i');
+    const promoteToQueen = document.createElement("i");
     promoteToQueen.className = `fas fa-chess-queen ${side}`;
     promoteToQueen.id = "queen";
 
-    [promoteToRook, promoteToKnight, promoteToBishop, promoteToQueen]
-    .forEach(el => icons.appendChild(el));
+    [promoteToRook, promoteToKnight, promoteToBishop, promoteToQueen].forEach(
+      el => icons.appendChild(el)
+    );
 
-    const coverBoard = document.createElement('div')
-    coverBoard.className = 'cover-board';
- 
-    document.querySelector('.wrap').appendChild(coverBoard);
-    document.querySelector('.wrap').appendChild(promotionBox);
+    const coverBoard = document.createElement("div");
+    coverBoard.className = "cover-board";
+
+    document.querySelector(".wrap").appendChild(coverBoard);
+    document.querySelector(".wrap").appendChild(promotionBox);
   }
 
-  promote() {
+  promote(board) {
     const x = this.piece._x;
     const y = this.piece._y;
     const side = this.piece._side;
-    const promotionBox = document.querySelector('.promotion-box')
-    const coverBoard = document.querySelector('.cover-board')
-    const icons = [...document.querySelector('.promotion-icons').children];
-    
+    const promotionBox = document.querySelector(".promotion-box");
+    const coverBoard = document.querySelector(".cover-board");
+    const icons = [...document.querySelector(".promotion-icons").children];
+
     // dodanie event listenerow
     icons.forEach(figure => {
-      figure.addEventListener("click", switchFigures)
+      figure.addEventListener("click", switchFigures(board));
     });
 
-    function switchFigures(e) {
+    function switchFigures(e, board) {
       let newFigure;
-      switch(e.target.id) {
+      switch (e.target.id) {
         case "knight":
           newFigure = new Knight(x, y, side);
           break;
@@ -76,19 +77,19 @@ class PromotePawn {
         case "queen":
           newFigure = new Queen(x, y, side);
           break;
-        default: break;
+        default:
+          break;
       }
 
       // podmiana pionka na nowa figure
       board[x][y] = newFigure;
-      
+
       // zmiana wyswietlanej ikony od razu po wybraniu nowej figury
-      document.querySelector(
-        `[data-id="${x}-${y}"]`
-      ).innerHTML = newFigure.display;
-      
-      document.querySelector('.wrap').removeChild(promotionBox);
-      document.querySelector('.wrap').removeChild(coverBoard);
+      document.querySelector(`[data-id="${x}-${y}"]`).innerHTML =
+        newFigure.display;
+
+      document.querySelector(".wrap").removeChild(promotionBox);
+      document.querySelector(".wrap").removeChild(coverBoard);
     }
   }
 }
